4d3709eb16bbd49a45b62b8eee87d142d20727ea
samisosa20 <1449@holbertonschool.com>
2020-08-03 21:49:10 +0000

    delete comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# Create a file
+# Creates a file in /tmp
 
 file { '/tmp/holberton':
-    ensure => 'present',
-    content => 'I love Puppet',
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0744',
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
