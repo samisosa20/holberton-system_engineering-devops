e3c4ff40049f5a8818d5e63aa0df81dc9999806c
samisosa20 <1449@holbertonschool.com>
2020-08-03 21:47:30 +0000

    delete comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file
 
-file { '/tmp/holberton': # the path of the new file
-    ensure => 'present', # just means that the resource should be a file, directory, or link; if it does not exist, create a file.
-    content => 'I love Puppet', # this text will be inside the file
+file { '/tmp/holberton':
+    ensure => 'present',
+    content => 'I love Puppet',
     owner => 'www-data',
     group => 'www-data',
-    mode => '0744', # permission
+    mode => '0744',
 }
